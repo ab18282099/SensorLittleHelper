f6164c6a0abcd9fd6db61292ac5501b2267bb40f
Jackson Lin <a182820998@gmail.com>
2017-11-20 01:08:35 +0800

    use interface FragmentMenuItemClickListener

---
+++ app/src/main/java/com/example/user/soil_supervise_kotlin/Interfaces/FragmentMenuItemClickListener.kt
@@ -0,0 +1,8 @@
+package com.example.user.soil_supervise_kotlin.Interfaces
+
+import android.view.MenuItem
+
+interface FragmentMenuItemClickListener
+{
+    fun FragmentMenuItemClickListenerObject() : (MenuItem) -> Boolean
+}
--- app/src/main/java/com/example/user/soil_supervise_kotlin/Interfaces/FragmentOptionsMenu.kt
+++
@@ -1,9 +0,0 @@
-package com.example.user.soil_supervise_kotlin.Interfaces
-
-import android.view.MenuItem
-
-interface FragmentOptionsMenu
-{
-    fun FragmentMenuResource() : Int
-    fun FragmentMenuItemClickListener() : (MenuItem) -> Boolean
-}
